0d47b30c61b9b154199a95653813b61e57a53302
paularmelo <paula.rmelo@gmail.com>
2021-12-12 19:35:22 -0500

    Modificação Main

--- ePet/instance/controllers/LoginViewController.swift
+++ ePet/instance/controllers/LoginViewController.swift
@@ -10,22 +10,15 @@ import UIKit
 class LoginViewController: UIViewController {
 
  
-    @IBOutlet weak var txtLogin: UITextField!
+    @IBOutlet weak var txtUser: UITextField!
     
-    @IBOutlet weak var txtPasseword: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
     
-    @IBAction func openAction(_ sender: Any) {
-    
-        if(txtLogin.text == "paula.rmelo@gmail.com" && txtPasseword.text == "1234"){
-          performSegue(withIdentifier: "Home", sender: "self")
-            
-        }else{
-            print("Nao foi possivel acessar")
-        }
+           
     
     
     
-    }
+ 
     
 
 }
