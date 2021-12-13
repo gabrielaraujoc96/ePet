1a464c797c4e00af30144f8c3a33963fdb209a53
Gabriel Araujo <gabriel.araujo@route.com>
2021-12-13 04:10:01 -0300

    finishing first part

--- ePet/instance/controllers/SearchViewController.swift
+++ ePet/instance/controllers/SearchViewController.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 class SearchViewController: UIViewController {
-    override func viewDidLoad() {
-        <#code#>
-    }
+//    override func viewDidLoad() {
+//        <#code#>
+//    }
 }
